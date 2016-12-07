a64fb815e74bc0fd0b58ed6a86f0afab2077d29a
Florian Nouviale <florian.nouviale@inria.fr>
2016-12-07 17:02:57 +0100

    damn IDE

--- bot.rb
+++ bot.rb
@@ -122,7 +122,7 @@ end
 
 def help
   %Q(I will respond to the following messages: \n
-      `wiki <search request> to search for something in the wiki`
+      `wiki <search request>` to search for something in the wiki.\n
       `bot hi` for a simple message.\n
       `bot attachment` to see a Slack attachment message.\n
       `@<your bot\'s name>` to demonstrate detecting a mention.\n
