7367554dc335cf8be865cb2b94f96002535c6592
Florian Nouviale <florian.nouviale@inria.fr>
2017-04-26 14:13:11 +0200

    azefqzefgbsrehntyj,uklio

--- commands/wiki.rb
+++ commands/wiki.rb
@@ -62,7 +62,8 @@ require 'logging'
 
         @@logger.debug("test2")
         @@logger.debug("response is : #{testResponse}")
-        @@logger.debug("response[query] is :"+ testResponse['query'])
+        zerfqzefzef = testResponse['query']
+        @@logger.debug("response[query] is :"+ zerfqzefzef)
         @@logger.debug("response[query][pages] is :"+ testResponse['query']['pages'])
         @@logger.debug("response[query][pages][0] is :+"+ testResponse['query']['pages'][0])
         @@logger.debug("response.data is : #{testResponse.data}")
