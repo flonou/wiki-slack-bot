833efde6e2bbe4d5b36bb819373f3a186108e678
Etienne Peillard <etienne.peillard@ec-nantes.fr>
2020-01-30 13:38:53 +0100

    New outstanding feature

---
+++ commands/nya.rb
@@ -0,0 +1,13 @@
+
+
+module Commands
+  class NyaBot
+    def initialize
+      facts = File.read("nyaFacts.txt").split
+    end
+    def randomFact(webclient)
+      fact = rand(0..facts.size-1)
+      webclient.chat_postMessage(channel: channel, text: fact, as_user: true)
+    end
+  end
+end
