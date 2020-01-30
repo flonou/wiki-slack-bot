bc7c2c283dd5a7fb8a680a86361d800ecec4eeb8
Florian Nouviale <florian.nouviale@inria.fr>
2020-01-30 14:19:29 +0100

    HA !

--- commands/nya.rb
+++ commands/nya.rb
@@ -3,7 +3,7 @@
 module Commands
   class NyaBot
     def initialize
-      facts = File.read(File.dirname(__FILE__) + "../resources/nyaFacts.txt").split
+      facts = File.read(File.dirname(__FILE__) + "/../resources/nyaFacts.txt").split
     end
     def randomFact(webclient)
       fact = rand(0..facts.size-1)
